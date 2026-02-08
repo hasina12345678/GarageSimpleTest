5388ddd6cd6cdf3811f3ea3427acc8afe60ead8f
Hasina <andrianirinahasina07@gmail.com>
2026-02-08 21:11:39 +0300

    mety amzay

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.garrage.app',
+  appId: 'com.garage.app',
   appName: 'GarageSimpleTest',
   webDir: 'www'
 };
 
 export default config;
 
-//mety kosa
+//mety
