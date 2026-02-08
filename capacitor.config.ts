92fa706b5c97e14a11aaf75a49cead33a6432a09
Hasina <andrianirinahasina07@gmail.com>
2026-02-08 20:54:25 +0300

    ntay ohh

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,5 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-// const config: CapacitorConfig = {
-//   appId: 'io.ionic.starter',
-//   appName: 'GarageSimpleTest',
-//   webDir: 'www'
-// };
-
-// export default config;
-
-
 const config: CapacitorConfig = {
   appId: 'com.garrage.app',
   appName: 'GarageSimpleTest',
