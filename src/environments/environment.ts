28e06d22d6b6eebf24a8cdf79d93e5a0e79f3127
Hasina <andrianirinahasina07@gmail.com>
2026-02-08 20:25:00 +0300

    aii kossaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
     storageBucket: "garrageapp-05.firebasestorage.app",
     messagingSenderId: "553426867703",
     appId: "1:553426867703:android:6e523ae4aa564197670af2"
-  },
-  vapidKey: "BJVonfohNgGsN7oFVtdnV-XOdyZP7YioY2dHOBpXQRCpW2uYIPIcs82l85dJcUnFIkTlgfAWRFTs_cf1kkdxmTY",
-  vercelUrl: 'https://ton-app.vercel.app'
+  }
+//   ,
+//   vapidKey: "BJVonfohNgGsN7oFVtdnV-XOdyZP7YioY2dHOBpXQRCpW2uYIPIcs82l85dJcUnFIkTlgfAWRFTs_cf1kkdxmTY",
+//   vercelUrl: 'https://ton-app.vercel.app'
 };
